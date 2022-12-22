aa3f2757e993b6b3a5f8c0f58adbe194856e98ff
TBYrgo <116157232+TBYrgo@users.noreply.github.com>
2022-12-22 11:39:56 +0100

    Add files via upload

--- setup.c
+++ setup.c
@@ -9,18 +9,12 @@ struct timer t0, t1; /* Timerkretsar. */
 struct tmp36 temp1;
 
 /********************************************************************************
-* setup: Initierar det inbyggda systemet. Lysdioder initieras på pin 8 - 9,
-*        tryckknappar initieras med aktiverade PCI-avbrott på pin 12 - 13
-*        och samtliga timerkretsar konfigureras, där timer 0 sätts till att
-*        löpa ut efter 300 ms (för att temporärt inaktivera PCI-avbrott på
-*        I/O-port B efter nedtryckning för att undvika multipla avbrott orsakat
-*        av kontaktstudsar, medan timer 1 - 2 sätts till att löpa ut efter
-*        100 ms (för blinkning via toggling av lysdioder).
+* setup: Initieringar av knapp med avbrott, Två timerkretsar med avbrott och 
+		 temperaturmätaren
 ********************************************************************************/
 void setup(void)
 {
 	
-/*Initieringar av knapp med avbrott, Två timerkretsar med avbrott och temperaturmätaren*/
 
 	button_init(&b1, 13);
 	button_enable_interrupt(&b1);
